59fae16757b4d3ba6cd53031ed097890ea7196d7
kkanakaprasad <109482694+kkanakaprasad@users.noreply.github.com>
2023-02-18 20:39:06 +0530

    basic schemas was completed

--- src/basic-details/schemas/basic-detail.schemas.ts
+++ src/basic-details/schemas/basic-detail.schemas.ts
@@ -5,16 +5,8 @@ import  mongoose  from 'mongoose';
 @Schema({
     timestamps: true,
 })
-export class Attachment {
-    @Prop({})
-    name: string;
-    @Prop({})
-    size: string;
-    @Prop({})
-    path: string;
-}
 
-export const AttachmentSchema = SchemaFactory.createForClass(Attachment)
+
 export class BasicDetail  {
   @Prop({ required: true, type: String })
   persons: string;
@@ -61,76 +53,6 @@ export class BasicDetail  {
   @Prop({ required: true, type: String })
   invoiceNo: string;
 
-  @Prop({ type:[AttachmentSchema] })
-  fileName: Attachment[];
-
   @Prop({ required: true, type: String })
   description: string;
-
-  @Prop({ required: true, type: String })
-  messagetoSmitha: string;
-
-  @Prop({ required: true, type: String })
-  messagetoPrem: string;
-
-  @Prop({ required: true, type: String })
-  nextAction: string;
-
-  @Prop({ required: true, type: String })
-  remarks: string;
-
-  @Prop({ required: true, type: String })
-  suppliersWhoProvidedQuoteTo: string;
-
-  @Prop({ required: true, type: String })
-  supplierName: string;
-
-  @Prop({ required: true, type: String })
-  contactName: string;
-
-  @Prop({ required: true, type: String })
-  email: string;
-
-  @Prop({ required: true, type: String })
-  phone: string;
-
-  @Prop({ required: true, type: String })
-  price: string;
-
-  @Prop({ required: true, type: String })
-  sAndH: string;
-
-  @Prop({ required: true, type: String })
-  cc: string;
-
-  @Prop({ required: true, type: String })
-  s_Total: string;
-
-  @Prop({ required: true, type: String })
-  leadTimeWK: string;
-
-  @Prop({ required: true, type: Boolean, default:false })
-  avalability: Boolean;
-
-  @Prop({ required: true, type: String })
-  totalcost: string;
-
-  @Prop({ required: true, type: Boolean, default:false })
-  sup: Boolean;
-
-  @Prop({ required: true, type: String })
-  shipCostMax: string;
-
-  @Prop({ required: true, type: String })
-  shipCostMin: string;
-
-  @Prop({ required: true, type: String })
-  waitingOn: string;
-
-  @Prop({ required: true, type: String })
-  profit: string;
-
-  @Prop({ required: true, type: String })
-  profitpercentage: string
-
 }
